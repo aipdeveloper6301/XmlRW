91d521e23b9bf7bc458bcc780f468636ee517a76
Developer <developer@davids-mac.local>
2016-10-28 05:24:08 +0200

    Initial Commit

---
+++ XMLtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMLtest
+//
+//  Created by RYG_Mac on 10/28/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMLtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMLtest
+//
+//  Created by RYG_Mac on 10/28/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
